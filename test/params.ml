e7706e26b36f5ab8115d8335b4d373ba9d374751
Mika Illouz <mika@illouz.net>
2023-06-20 12:14:32 -0700

    moved JSON config-file parsing from lib to test directory

--- test/params.ml
+++ test/params.ml
@@ -65,6 +65,7 @@ let read_and_set ?path env =
   match read ?path () with
   | Error s -> Error s
   | Ok t -> (
+    let open Gurobi in
     let failures = [] in
     let failures =
       List.fold_left
